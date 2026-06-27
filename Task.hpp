6f4f5543befac333cc3cfa96b645d958e7cc71f0
Rivial104 <rivial104i300@gmail.com>
2026-06-27 08:25:24 +0200

    Read tasks from file

--- Task.hpp
+++ Task.hpp
@@ -6,7 +6,9 @@
 class Task
 {
 public:
-    Task(int id, char* desc, int type, int prio) 
+    Task() = default;
+
+    Task(int id, std::string desc, int type, int prio) 
     {
         _params.id = id;
         _params.desc = desc;
@@ -14,12 +16,17 @@ public:
         _params.priority = prio;
     };
 
-    ~Task() {};
+    ~Task() = default;
     
     void print()
     {
-        printf("|| Task: %i || %s || Type: %i || Priority: %i ||\n", _params.id, _params.desc,  _params.type,  _params.priority);
+        printf("|| Task: %i || %s || Type: %i || Priority: %i ||\n", _params.id, _params.desc.c_str(),  _params.type,  _params.priority);
     }
+
+    void set_id(int i) {_params.id = i;};
+    void set_desc(std::string s) {_params.desc = s;};
+    void set_type(int t) {_params.type = t;};
+    void set_priority(int p) {_params.priority = p;};
 
 private:
 
@@ -45,8 +52,8 @@ private:
 
     struct TaskParameters
     {
-        int id;
-        char* desc;
+        int id{0};
+        std::string desc;
         int type{0};
         int priority{0};
 
